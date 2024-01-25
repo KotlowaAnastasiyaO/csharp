948cfbf679bdb26bbc41928b4ed16652fe6362bb
KotlowaAO <ya@kotlowaa.ru>
2024-01-25 21:29:29 +0300

    add lesson1:task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 2;
+int b = 6;
+int c = 8;
+int e = 3;
+int d = 11;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (e > max)
+{
+    max = e;
+}
+if (d > max)
+{
+    max = d;
+}
+Console.WriteLine(max);
